8aafc831430c067da7614c41a3fc744e66548317
Chad Roberts <croberts@redhat.com>
2016-11-14 10:51:13 -0500

    bumping version of example jar

--- smoketest.sh
+++ smoketest.sh
@@ -26,6 +26,6 @@ echo "Waiting $PAUSEFORENV seconds for environment variable update to go through
 sleep $PAUSEFORENV
 WORKERPOD=`oc get pods | grep -m 1 "${CLUSTERNAME}-w" | sed 's/\s\+/ /g' | cut -d' ' -f1`
 echo "Going to run smoke test on POD: $WORKERPOD"
-echo "oc exec ${WORKERPOD} -- spark-submit --master spark://$SPARKMASTER  --class org.apache.spark.examples.SparkPi /opt/spark/examples/jars/spark-examples_2.11-2.0.0-preview.jar 100"
+echo "oc exec ${WORKERPOD} -- spark-submit --master spark://$SPARKMASTER  --class org.apache.spark.examples.SparkPi /opt/spark/examples/jars/spark-examples_2.11-2.0.1.jar 100"
 
-oc exec ${WORKERPOD} -- /opt/spark/bin/spark-submit --master spark://$SPARKMASTER  --class org.apache.spark.examples.SparkPi /opt/spark/examples/jars/spark-examples_2.11-2.0.0.jar 100
+oc exec ${WORKERPOD} -- /opt/spark/bin/spark-submit --master spark://$SPARKMASTER  --class org.apache.spark.examples.SparkPi /opt/spark/examples/jars/spark-examples_2.11-2.0.1.jar 100
