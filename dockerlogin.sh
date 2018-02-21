be11739754ec529d513ab2bae9bd5cdcb7565a2f
Chad Roberts <croberts@redhat.com>
2018-02-21 13:37:53 -0500

    Updating for 3.9 output

--- dockerlogin.sh
+++ dockerlogin.sh
@@ -5,7 +5,7 @@ TOKEN=`oc whoami -t`
 
 oc login -u system:admin > /dev/null
 oc project default > /dev/null 2>&1
-REGIP=`oc get svc docker-registry | grep docker-registry | sed 's/\s\+/ /g' | cut -d' ' -f2`
+REGIP=`oc get svc docker-registry | grep docker-registry | sed 's/\s\+/ /g' | cut -d' ' -f3`
 REPO=$REGIP:5000
 
 docker login -u developer -e me@mine.com -p $TOKEN $REPO
--- getreg.sh
+++ getreg.sh
@@ -2,5 +2,5 @@
 
 oc login -u system:admin > /dev/null
 oc project default > /dev/null 2>&1
-REGIP=`oc get svc docker-registry | grep docker-registry | sed 's/\s\+/ /g' | cut -d' ' -f2`
+REGIP=`oc get svc docker-registry | grep docker-registry | sed 's/\s\+/ /g' | cut -d' ' -f3`
 echo $REGIP:5000
